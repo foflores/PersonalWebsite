1d040af7b6b687f34852c44b44f259b4e4480610
Favian Flores <floresf1997@gmail.com>
2021-11-04 21:34:34 -0400

    moved files

--- src/settings.py
+++ src/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'src.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'local/db.sqlite3',
     }
 }
 
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'local/media')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
