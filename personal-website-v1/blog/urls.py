b4afbafb7303ca9305869f8429008837f9cd157a
Favian Flores <floresf1997@gmail.com>
2023-08-20 20:22:37 -0400

    delete v1

--- personal-website-v1/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from blog import views
-from django.conf.urls.static import static
-from django.conf import settings
-
-
-urlpatterns = [
-	path('', views.blogHome, name='blogHome'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- personal-website-v1/config/hosts.py
+++
@@ -1,9 +0,0 @@
-from django_hosts import patterns, host
-from django.contrib import admin
-
-
-host_patterns = patterns('',
-	host("admin", "config.urls", name="admin"),
-	host("blog", "blog.urls", name="blog"),
-	host("projects", "projects.urls", name="projects"),
-)
